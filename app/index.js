a96a00e8f04ec8e8905efa53468d4f65e11c9d6e
Manav Misra <manavm@visionwebsoft.com>
2023-02-17 10:35:38 -0600

    Do the 'easy counting' stuff to pass a test

--- app/index.js
+++ app/index.js
@@ -1,1 +1,10 @@
-export const add2Nums = (num1, num2) => num1 + num2;
+export const isValidBracketsBracesParentheses = (str) => {
+  // Get rid of the each and every character that is not a bracket, brace or parenthesis
+  const bracketBraceParenthesisOnly = str.replace(/[^(){}[\]]/g, "");
+
+  // If the string is empty, return true
+  if (!bracketBraceParenthesisOnly) return true;
+
+  // If the string has an odd length, return false
+  if (bracketBraceParenthesisOnly.length % 2 !== 0) return false;
+};
